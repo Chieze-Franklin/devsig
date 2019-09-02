6f84345a801aab8c21713ccae6cb03618579049f
Franklin Chieze <chieze.franklin@gmail.com>
2019-09-02 21:07:59 +0100

    create slack monitor

--- monitors/slack.js
+++ monitors/slack.js
@@ -1,18 +1,54 @@
-/*
-event = {}
-on key event:
-  if active win is slack (or chrome with slack as title)
-    if (event.lastType && event.lastType - now() > 30 seconds)
-      event = {}
-    else 
-      event.lastType = now()
-    if key is enter:
-      if event.typing: // ensure shift + enter doesn't make this true
-        message = { start: event.start, end: date, channel } //any way of knowing if its public or private?
-        log(message)
-        event.typing = false
-    else:
-      if !event.typing:
-        event.typing = true
-        event.start = date
- */
+const activeWin = require('active-win');
+const iohook = require('iohook');
+
+const log = require('../services/log');
+
+let activeEvent = {};
+let logger;
+
+async function eventHandler(event) {
+  const window = await activeWin();
+  if (!window) {
+    return;
+  }
+  const isSlack = window.owner.bundleId === 'com.tinyspeck.slackmacgap' ||
+    window.title.startsWith('Slack | ');
+  if (isSlack) {
+    const now = new Date();
+    if (activeEvent.lastTyped && (now.getTime() - (activeEvent.lastTyped.getTime()) > 30000)) {
+      activeEvent = {};
+    }
+    const { shiftKey, altKey, ctrlKey, metaKey, keycode, rawcode, type } = event;
+    const keyIsEnter = (!shiftKey && !altKey && !ctrlKey && !metaKey &&
+    keycode === 28 && rawcode === 36 && type === 'keydown');
+    if (keyIsEnter) {
+      if (activeEvent.typing) {
+        const titleParts = window.title.split('|').map(part => part.trim());
+        logger.info({
+          workspace: titleParts[2],
+          channel: titleParts[1],
+          event: 'typing',
+          start: activeEvent.start,
+          end: now
+        });
+        activeEvent = {};
+      }
+    } else {
+      if (!activeEvent.start) {
+        activeEvent.start = now;
+        activeEvent.typing = true;
+      }
+      activeEvent.lastTyped = now;
+    }
+  }
+}
+
+iohook.on('keydown', (event) => eventHandler(event));
+
+iohook.name = 'slack';
+iohook.init = (options) => {
+  logger = log.getLogger('slack');
+  iohook.emit('start', 'slack');
+}
+
+module.exports = iohook;
