4a8cfbc9ed6fe31724c4625135049bcac07b4080
Franklin Chieze <chieze.franklin@gmail.com>
2019-09-02 15:27:56 +0100

    add check for duration between typings

--- monitors/slack.js
+++ monitors/slack.js
@@ -2,8 +2,12 @@
 event = {}
 on key event:
   if active win is slack (or chrome with slack as title)
+    if (event.lastType && event.lastType - now() > 30 seconds)
+      event = {}
+    else 
+      event.lastType = now()
     if key is enter:
-      if event.typing:
+      if event.typing: // ensure shift + enter doesn't make this true
         message = { start: event.start, end: date, channel } //any way of knowing if its public or private?
         log(message)
         event.typing = false
