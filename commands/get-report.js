d763f1208cc419f2f5f34e95e15e195afbcb1e29
Franklin Chieze <chieze.franklin@gmail.com>
2019-09-08 00:52:07 +0100

    refactor reporting

--- commands/get-report.js
+++ commands/get-report.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk');
+const dayjs = require('dayjs');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
+const open = require('open');
 const path = require('path');
 const { table } = require('table');
 
@@ -8,6 +10,7 @@ const { log } = console;
 const { blue, blueBright, green, greenBright, red, redBright, yellow, yellowBright } = chalk;
 
 let reporterFiles;
+const sessionName = dayjs().format('YYYY-MM-DDTHH-mm-ss.SSS-A') + '.txt';
 
 module.exports = function(reporter, options) {
   try {
@@ -15,37 +18,43 @@ module.exports = function(reporter, options) {
       reporterFiles = fs.readdirSync(path.join(__dirname, '../reporters/'));
     }
 
-    // get reporter to run
-    const reporterFileToRun = reporterFiles
-      .find(file => file.toLowerCase() === reporter.toLowerCase() ||
+    // get reporters to run
+    const reportersFileToRun = reporterFiles
+      .filter(file => typeof reporter === 'undefined' ||
+      file.toLowerCase() === reporter.toLowerCase() ||
       file.toLowerCase() === reporter.toLowerCase() + '.js');
 
-    if (!reporterFileToRun) {
+    if (reportersFileToRun.length === 0) {
       throw new Error(`Cannot find reporter '${reporter}'`);
     }
 
-    const rpt = require(`../reporters/${reporterFileToRun}`);
-    rpt.on('report', (msg) => {
-      if (msg.output === 'console') {
-        const output = table(msg.data, msg.options); // this should not be here
-        console.log(output);
-      } else if (msg.output === 'browser') {
-        // TODO
-      } else if (msg.output === 'file') {
-        // create report directory
-        mkdirp.sync(path.join(__dirname, `../reports`));
-        fs.appendFile(__dirname, `../reports/${options.file || 'report.txt'}`, msg.data + '\n\n', (error) => {});
-        // TODO open the file in default editor
-      }
+    reportersFileToRun.forEach(r => {
+      const rpt = require(`../reporters/${r}`);
+      rpt.on('report', (msg) => {
+        if (msg.output === 'console') {
+          const output = table(msg.data, msg.options); // this should not be here
+          console.log(output);
+        } else if (msg.output === 'browser') {
+          // open the url in default program
+          open(msg.data);
+        } else if (msg.output === 'file') {
+          // create report directory
+          mkdirp.sync(path.join(__dirname, `../reports`));
+          fs.appendFile(path.join(__dirname, `../reports/${options.file || sessionName}`), msg.data + '\n\n', (error) => {});
+          // open the file in default program
+          open(path.join(__dirname, `../reports/${options.file || sessionName}`));
+        }
+      });
+      const name = rpt.name || r;
+      rpt.on('error', (msg) => log(redBright(`[${name}] ${msg}`)));
+      rpt.on('failure', (msg) => log(redBright(`[${name}] ${msg}`)));
+      rpt.on('info', (msg) => log(blueBright(`[${name}] ${msg}`)));
+      rpt.on('start', (msg) => log(`started monitor: ${yellow(name)}`));
+      rpt.on('success', (msg) => log(greenBright(`[${name}] ${msg}`)));
+      rpt.on('warning', (msg) => log(yellowBright(`[${name}] ${msg}`)));
+      rpt.init(options);
+      rpt.start();
     });
-    const name = rpt.name || reporterFileToRun;
-    rpt.on('error', (msg) => log(redBright(`[${name}] ${msg}`)));
-    rpt.on('failure', (msg) => log(redBright(`[${name}] ${msg}`)));
-    rpt.on('info', (msg) => log(blueBright(`[${name}] ${msg}`)));
-    rpt.on('success', (msg) => log(greenBright(`[${name}] ${msg}`)));
-    rpt.on('warning', (msg) => log(yellowBright(`[${name}] ${msg}`)));
-    rpt.init(options);
-    rpt.start();
   } catch (error) {
     log(redBright(error.message));
     log(error);
