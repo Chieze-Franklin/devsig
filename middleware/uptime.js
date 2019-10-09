4dbbee8906b47df0d703d73e5fba3f5f4641d80b
Franklin Chieze <chieze.franklin@gmail.com>
2019-10-09 23:56:58 +0100

    ...

--- middleware/uptime.js
+++ middleware/uptime.js
@@ -4,23 +4,3 @@
 // in a cron job that runs every hour:
 // client.period('day').add().send('devsig_uptime', 1)
 // 1. implement the cloud function logic for this 2. implement the devsig logic for this 3. bring it together here
-
-// deployment:
-//     slack:
-//         replicas: 4
-//         scale:
-//             minReqPerMin: 2
-//             maxReqPerMin: 10
-// services:
-//     slack:
-//         image: slack:3
-//         command: --default-authentication-plugin=mysql_native_password
-//         environment:
-//             PORT: 3000
-//         volumes:
-//             ./:/user/app
-//         ports:
-//             - 3000
-//         routes:
-//             - /api/v1/slack
-//             - /api/v2/slack
